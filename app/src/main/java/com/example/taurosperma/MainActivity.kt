f4226b0c876719e887ae48ee78966f374c6e2d5e
Justinas23 <justixx1995@gmail.com>
2021-09-04 22:18:34 +0300

    Added to function

--- app/src/main/java/com/example/taurosperma/MainActivity.kt
+++ app/src/main/java/com/example/taurosperma/MainActivity.kt
@@ -28,7 +28,8 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
     Text( text = "penis $name!")
-}   Text( text = " namas ")
+    Text( text = " namas ")
+}
 
 @Preview(showBackground = true)
 @Composable
